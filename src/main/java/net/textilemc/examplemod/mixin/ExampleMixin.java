27636d62ccb24926e4976004229e42352d096570
PheonixVX <TheGrimReaper.VX@gmail.com>
2021-02-04 19:08:04 -0500

    Forgot to fix mixin

--- src/main/java/net/textilemc/examplemod/mixin/ExampleMixin.java
+++ src/main/java/net/textilemc/examplemod/mixin/ExampleMixin.java
@@ -1,23 +1,20 @@
 package net.textilemc.examplemod.mixin;
 
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.q;
-
-import java.util.Arrays;
-
-@Mixin(q.class)
+@Mixin(TitleScreen.class)
 public abstract class ExampleMixin {
 
     @Shadow
-    private String j;
+    private String field_1103;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onInit(CallbackInfo ci) {
-        j = "Infdev 227 woop!!";
+        field_1103 = "Infdev 227 woop!!";
     }
 }
