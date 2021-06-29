1c8beb5f94d714cf30cfdd841baba01026ef6df3
MicherGit <40839581+TwelvianZeta@users.noreply.github.com>
2021-06-29 18:07:05 -0400

    set version to 223

--- src/main/java/net/textilemc/examplemod/mixin/ExampleMixin.java
+++ src/main/java/net/textilemc/examplemod/mixin/ExampleMixin.java
@@ -1,13 +1,13 @@
 package net.textilemc.examplemod.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(GuiMainMenu.class)
 public abstract class ExampleMixin {
 
     @Shadow
